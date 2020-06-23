a7154d960aa2796ebeb724d2ecc19ae14904ea43
jetsetPanda <44765030+jetsetPanda@users.noreply.github.com>
2020-06-23 16:10:48 -0400

    base component

--- pages/experiences/index.vue
+++ pages/experiences/index.vue
@@ -1,3 +1,30 @@
 <template>
-  <h1>UP Galactic Experiences</h1>
+  <section class="experiences">
+    <h1 class="fontGalactic">UP Galactic Experiences</h1>
+    <p>Travel the galaxy in style, let us show you the way.</p>
+    <article class="experience">
+      <div></div>
+      <h1>Naboo Theed Palace High Tea</h1>
+      <p>Culinary splendour in the dramatic backdrop of one of the galaxy's most beautiful imperial palaces.</p>
+    </article>
+  </section>
 </template>
+
+<style scoped>
+  .experiences {
+
+  }
+
+  .fontGalactic {
+    color: #214774;
+    font-size: 3rem;
+  }
+
+  .experience {
+    box-sizing: border-box;
+    width: 320px;
+    padding: 10px;
+    border: 1px solid #CCCCCC;
+    box-shadow: 1px 2px 3px #214774;
+  }
+</style>
