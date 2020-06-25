287a7abba342eca3a023ad330f94c94f6a04aa64
jetsetPanda <44765030+jetsetPanda@users.noreply.github.com>
2020-06-25 18:38:20 -0400

    journey component

---
+++ components/Journey.vue
@@ -0,0 +1,17 @@
+<template>
+  <article class="journey">
+    <div
+      class="thumbnail"
+      :style="{backgroundImage: 'url(' + thumbnail + ')'}"
+    ></div>
+    <h1>{{ journeyName }}</h1>
+    <h4>{{ routeName }}</h4>
+    <p>{{ description }}</p>
+  </article>
+</template>
+
+<script>
+  export default {
+    props: ['thumbnail, journeyName, routeName, description']
+  }
+</script>
