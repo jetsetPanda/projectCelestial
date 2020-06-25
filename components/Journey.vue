08f681c3c91f809bfa59568ef53794c80547eed4
jetsetPanda <44765030+jetsetPanda@users.noreply.github.com>
2020-06-25 18:51:33 -0400

    journey component css

--- components/Journey.vue
+++ components/Journey.vue
@@ -6,12 +6,35 @@
     ></div>
     <h1>{{ journeyName }}</h1>
     <h4>{{ routeName }}</h4>
-    <p>{{ description }}</p>
+    <p>{{ journeyDescription }}</p>
   </article>
 </template>
 
 <script>
   export default {
-    props: ['thumbnail, journeyName, routeName, description']
+    props: ['thumbnail', 'journeyName', 'routeName', 'journeyDescription']
   }
 </script>
+
+
+<style scoped>
+  .journey {
+    margin: 17px;
+    box-sizing: border-box;
+    width: 350px;
+    padding: 25px;
+    border: 1px solid #CCCCCC;
+    border-radius: 10%;
+    box-shadow: 1px 2px 3px #214774;
+  }
+
+  .thumbnail {
+    background-position: center;
+    background-size: cover;
+    height: 100%;
+    height: 200px;
+    border-radius: 2%;
+    box-shadow: 1px 1px 1px #47494E;
+  }
+
+</style>
