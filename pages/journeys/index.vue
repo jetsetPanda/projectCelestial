4801756dc076913477b3fea81007214919009c4c
jetsetPanda <44765030+jetsetPanda@users.noreply.github.com>
2020-06-25 23:43:34 -0400

    data bind from API/demo data

--- pages/journeys/index.vue
+++ pages/journeys/index.vue
@@ -4,33 +4,27 @@
     <h2>The journey itself is the destination.</h2>
     <section class="journeys">
 
+<!--      bind values to dynamic vals in API/demo data-->
       <Journey
-        thumbnail="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/770814e0-a7c0-4e13-aa31-107aed4dbaf6/dbpkbbj-5ff9a3d5-68cc-49d1-b79a-02bea83c7a93.jpg/v1/fill/w_1600,h_860,q_75,strp/mon_calamari_shipyards_mc85_command_cruiser_by_shoguneagle_dbpkbbj-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD04NjAiLCJwYXRoIjoiXC9mXC83NzA4MTRlMC1hN2MwLTRlMTMtYWEzMS0xMDdhZWQ0ZGJhZjZcL2RicGtiYmotNWZmOWEzZDUtNjhjYy00OWQxLWI3OWEtMDJiZWE4M2M3YTkzLmpwZyIsIndpZHRoIjoiPD0xNjAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.TjAqcVXnuY3fqlR3A0wwSGkdtuhtBxJ5dhfDaPlYB50"
-        journeyName="The Mon Calamari"
-        journeyTagline="Pampering, Rebel Style"
-        routeName="Coruscant - Endor"
-        journeyDescription="Travel in sheer luxury to a galaxy far far away with the Rebel Alliance's most famous frigate, now converted to a supremely relaxing resort cruiser."
-        journeyid="moncalamari"
+        v-for="journey in FXjourneys"
+        :key="journey.journeyid"
+        :thumbnail="journey.thumbnail"
+        :journeyName="journey.journeyName"
+        :journeyTagline="journey.journeyTagline"
+        :routeName="journey.routeName"
+        :journeyDescription="journey.journeyDescription"
+        :journeyid="journey.journeyid"
       />
 
-      <Journey
-        thumbnail="https://i.pinimg.com/564x/f5/28/08/f528084ac028c2af341c613298e68eb3.jpg"
-        journeyName="Fhloston Paradise"
-        journeyTagline="Azure Escapade"
-        routeName="Mondoshawar - Earth 2263"
-        journeyDescription="Hosted by the voicecorder stylings of MC Ruby Rod, expect a fun and sensual lightwarp through the stars in this stylish vessel. The main highlight will be the showstopping performance by the Singular Azure Diva them-theyselves, Plavalaguna."
-        journeyid="fhlostonparadise"
-      />
+<!--      <Journey-->
+<!--        thumbnail="http://st-intelligence.com/ship_database/fed/images/Vcity_main.jpg"-->
+<!--        journeyName="The Voth"-->
+<!--        journeyTagline="City on a Ship"-->
 
-      <Journey
-        thumbnail="http://st-intelligence.com/ship_database/fed/images/Vcity_main.jpg"
-        journeyName="The Voth"
-        journeyTagline="City on a Ship"
-
-        routeName="Earth, UFP - Delta Quadrant"
-        journeyDescription="Best known for capturing the Federation starship USS Voyager in 2373, this citadel class starship can set you to stun with its majestic gravity simulated avenues and cityscapes."
-        journeyid="voth"
-      />
+<!--        routeName="Earth, UFP - Delta Quadrant"-->
+<!--        journeyDescription="Best known for capturing the Federation starship USS Voyager in 2373, this citadel class starship can set you to stun with its majestic gravity simulated avenues and cityscapes."-->
+<!--        journeyid="voth"-->
+<!--      />-->
 
     </section>
   </section>
@@ -47,8 +41,9 @@ export default {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         resolve({
-          journeys: [
+          FXjourneys: [
             {
+              id: "1",
               journeyid: "calamari",
               journeyName: "The Mon Calamari",
               journeyTagline: "Pampering, Rebel Style",
@@ -57,14 +52,16 @@ export default {
               thumbnail: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/770814e0-a7c0-4e13-aa31-107aed4dbaf6/dbpkbbj-5ff9a3d5-68cc-49d1-b79a-02bea83c7a93.jpg/v1/fill/w_1600,h_860,q_75,strp/mon_calamari_shipyards_mc85_command_cruiser_by_shoguneagle_dbpkbbj-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD04NjAiLCJwYXRoIjoiXC9mXC83NzA4MTRlMC1hN2MwLTRlMTMtYWEzMS0xMDdhZWQ0ZGJhZjZcL2RicGtiYmotNWZmOWEzZDUtNjhjYy00OWQxLWI3OWEtMDJiZWE4M2M3YTkzLmpwZyIsIndpZHRoIjoiPD0xNjAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.TjAqcVXnuY3fqlR3A0wwSGkdtuhtBxJ5dhfDaPlYB50",
             },
             {
+              id: "2",
               journeyid: "fhloston",
               journeyName: "Fhloston Paradise",
               journeyTagline: "Azure Escapade",
               routeName: "Mondoshawar - Earth 2263",
-              journeyDescription: "Hosted by the voicecorder stylings of MC Ruby Rod, expect a fun and sensual lightwarp through the stars in this stylish vessel. The main highlight will be the showstopping performance by the Singular Azure Diva them-theyselves, Plavalaguna.",
+              journeyDescription: "Graced with the voicecorder stylings of MC Ruby Rod, expect a fun and sensual lightwarp through the stars in this stylish vessel. The main highlight will be the showstopping performance by the Singular Azure Diva them-theyselves, Plavalaguna.",
               thumbnail: "https://i.pinimg.com/564x/f5/28/08/f528084ac028c2af341c613298e68eb3.jpg",
             },
             {
+              id: "3",
               journeyid: "voth",
               journeyName: "The Voth",
               journeyTagline: "City on a Ship",
@@ -72,7 +69,6 @@ export default {
               journeyDescription: "Best known for capturing the Federation starship USS Voyager in 2373, this citadel class starship can set you to stun with its majestic gravity simulated avenues and cityscapes.",
               thumbnail: "http://st-intelligence.com/ship_database/fed/images/Vcity_main.jpg",
             },
-
           ]
         })
       }, 1500)
