6393f9565331edd1b7598b9482eb15fcd80dc407
jetsetPanda <44765030+jetsetPanda@users.noreply.github.com>
2020-06-26 04:41:45 -0400

    init nav component

---
+++ components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li><nuxt-link to="/">Home</nuxt-link></li>
+        <li><nuxt-link to="/journeys">Journeys</nuxt-link></li>
+        <li><nuxt-link to="/experiences">Experiences</nuxt-link></li>
+        <li><nuxt-link to="/about">About Us</nuxt-link></li>
+        <li><nuxt-link to="/">Member Login</nuxt-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+
+<style scoped>
+  header {
+    width: 100%;
+    height: 40px;
+    background-color: #2D4266;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  a {
+    text-decoration: none;
+    font-size: 1.3em;
+    color: #FFFFFF;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  li {
+    margin: 0 12px;
+  }
+
+</style>
