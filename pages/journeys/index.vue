0a481eae88eef3dd30b7c51b453a7251b37f4a6d
jetsetPanda <44765030+jetsetPanda@users.noreply.github.com>
2020-06-25 23:23:26 -0400

    demo data

--- pages/journeys/index.vue
+++ pages/journeys/index.vue
@@ -37,13 +37,48 @@
 </template>
 
 <script>
-  import Journey from '@/components/Journey'
+import Journey from '@/components/Journey'
 
-  export default {
-    components: {
-      Journey
-    }
-  }
+export default {
+  components: {
+    Journey
+  },
+  asyncData() { //simulate an async req
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          journeys: [
+            {
+              journeyid: "calamari",
+              journeyName: "The Mon Calamari",
+              journeyTagline: "Pampering, Rebel Style",
+              routeName: "Coruscant - Endor",
+              journeyDescription: "Travel in sheer luxury to a galaxy far far away with the Rebel Alliance's most famous frigate, now converted to a supremely relaxing resort cruiser.",
+              thumbnail: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/770814e0-a7c0-4e13-aa31-107aed4dbaf6/dbpkbbj-5ff9a3d5-68cc-49d1-b79a-02bea83c7a93.jpg/v1/fill/w_1600,h_860,q_75,strp/mon_calamari_shipyards_mc85_command_cruiser_by_shoguneagle_dbpkbbj-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD04NjAiLCJwYXRoIjoiXC9mXC83NzA4MTRlMC1hN2MwLTRlMTMtYWEzMS0xMDdhZWQ0ZGJhZjZcL2RicGtiYmotNWZmOWEzZDUtNjhjYy00OWQxLWI3OWEtMDJiZWE4M2M3YTkzLmpwZyIsIndpZHRoIjoiPD0xNjAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.TjAqcVXnuY3fqlR3A0wwSGkdtuhtBxJ5dhfDaPlYB50",
+            },
+            {
+              journeyid: "fhloston",
+              journeyName: "Fhloston Paradise",
+              journeyTagline: "Azure Escapade",
+              routeName: "Mondoshawar - Earth 2263",
+              journeyDescription: "Hosted by the voicecorder stylings of MC Ruby Rod, expect a fun and sensual lightwarp through the stars in this stylish vessel. The main highlight will be the showstopping performance by the Singular Azure Diva them-theyselves, Plavalaguna.",
+              thumbnail: "https://i.pinimg.com/564x/f5/28/08/f528084ac028c2af341c613298e68eb3.jpg",
+            },
+            {
+              journeyid: "voth",
+              journeyName: "The Voth",
+              journeyTagline: "City on a Ship",
+              routeName: "Earth, UFP - Delta Quadrant",
+              journeyDescription: "Best known for capturing the Federation starship USS Voyager in 2373, this citadel class starship can set you to stun with its majestic gravity simulated avenues and cityscapes.",
+              thumbnail: "http://st-intelligence.com/ship_database/fed/images/Vcity_main.jpg",
+            },
+
+          ]
+        })
+      }, 1500)
+    })
+  },
+};
 </script>
 
 <style scoped>
