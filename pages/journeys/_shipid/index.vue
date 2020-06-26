7efbadcfd74a27d8bfd85efd10d244f61815dfcf
jetsetPanda <44765030+jetsetPanda@users.noreply.github.com>
2020-06-25 20:30:49 -0400

    init dynamic route

---
+++ pages/journeys/_shipid/index.vue
@@ -0,0 +1,22 @@
+<template>
+<!--  e.g. below: dynamic route in vue router-->
+<!--  <p>{{ $route.params.shipid }}</p>-->
+  <section class="single-journey">
+    <h1>Journey Name:</h1>
+    <div>
+      <img src="" alt="">
+    </div>
+    <p>Ship description</p>
+  </section>
+</template>
+
+<style scoped>
+
+  .single-journey {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    padding: 35px;
+  }
+</style>
