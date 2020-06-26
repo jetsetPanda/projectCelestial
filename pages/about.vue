0ad29a16aebc883095db0a8e2186abd79d0be8c9
jetsetPanda <44765030+jetsetPanda@users.noreply.github.com>
2020-06-26 04:45:09 -0400

    about view

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,41 @@
 <template>
-  <h1>About UP Galactic</h1>
+  <section>
+    <section>
+      <Heading
+        headerName="UpGalactic Origins"
+      />
+    </section>
+    <section>
+      <Heading
+        subHeaderName="Part 0: The Spark of Hope"
+      />
+    </section>
+    <section class="about">
+      <p>It is the year 2043. The global petroleum ban has been imposed for a half decade, following the dual cataclysms of the Final Ozone Breach and the CovSARS-37 pandemic. After the overthrow of Prophet Kushner's regency, newly installed President Kaepernick kicked off Project N3D (The New New New Deal), with the hopes of revitalizing the ravaged economy of the Union States of Coastal America. There was a massive civic effort of the surviving population in harvesting and repurposing scrap materials after the Overthrow, including millions of fossil fuel vehicles and the ruined trans coastal Hyperloop. It was at this confluence of events that our visionary founder, Jetset Zero:One, with the help of a small Project N3D ethereum research grant started drafting plans to utilize the newly unlocked power of cryo-fusion. Zero:One's unwavering goal was to have the good ole' U.S.C.A. beat Albania, The Donaldian Confederacy, and Xi'an-Tibet in finally reaching the distant stars......</p>
+    </section>
+
+    <section>
+      <Heading
+        subHeaderName="Part 1: The Twelfth Moon of Uranus"
+      />
+<!--      <section class="about">-->
+<!--        <p>Six hundred earth-solar cycles have passed since the Overthrow. Woael Rojhnston just left the familiar comfort of his appleFluidPodXS to project a disney onto his corporeal neuronetwork.</p>-->
+<!--      </section>-->
+    </section>
+  </section>
+
+
 </template>
+
+<style scoped>
+  .about {
+    margin: 6vh 15vw;
+    display: flex;
+    flex-flow: wrap;
+    justify-content: center;
+    align-items: stretch;
+    font-size: xx-large;
+  }
+
+
+</style>
